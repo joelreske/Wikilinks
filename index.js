b566d3a7bae7eabe3da16f46e604fa4729143ad3
Toby Glover <william.glover@tufts.edu>
2016-11-03 13:22:42 -0400

    setting up node project

---
+++ index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// Taken from Ming's example code
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+var mongoUri = process.env.MONGODB_URI;
+var MongoClient = require('mongodb').MongoClient;
+var db = MongoClient.connect(mongoUri, function(error, databaseConnection) {
+	db = databaseConnection;
+});
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+	response.render('pages/index';
+});
+
+app.listen(app.get('port'), function() {
+ 	console.log('Node app is running on port', app.get('port'));
+});
+
+function enableCORS(response) {
+	response.header("Access-Control-Allow-Origin", "*");
+  	response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+}
