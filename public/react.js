ac1935ad198185529dbfb8e84d91192cd1e09e3f
rgalbiati <raina.galbiati@tufts.edu>
2016-11-11 14:59:45 -0500

    logo is coded but not rendering

--- public/react.js
+++ public/react.js
@@ -1,1 +1,17 @@
-react.js
+<script type="text/jsx">
+
+// var React = require('react');
+// var ReactDOM = require('react-dom');
+
+var logo = React.createClass({
+    render : function(){
+        return (
+            <h1>WikiLinks</h1>
+        );
+    } 
+
+});
+
+React.render(<logo/>,document.getElementById('logo'));
+
+</script>
