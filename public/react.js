3d1c3f65270ff8c89f37e21c7c1e02f0c1df1122
Ashley Smith <Ashley@Ashleys-MacBook-Pro-5.local>
2016-11-11 14:28:07 -0500

    worked on adding start button

--- public/react.js
+++ public/react.js
@@ -1,1 +1,17 @@
 react.js
+
+var React = require('react');
+var Button = require('react-native-button');
+
+var Button = React.createClass({
+  render: function () {
+    return (
+      <button
+        className="startbutton"
+        style={startButtonStyle}
+        onClick={this.props.handleClick}>Start Game</button>
+    );
+  }
+});
+
+module.exports = Button;
