17375124989bcdf2645d234f67b90a6edda43dd5
rgalbiati <raina.galbiati@tufts.edu>
2016-11-24 23:53:40 -0500

    starting screen, next step would be to combine text and button into one object

--- public/react.jsx
+++ public/react.jsx
@@ -7,20 +7,24 @@ var Logo = React.createClass({
 ReactDOM.render(<Logo/>, document.getElementById('header'));
 
 
-
+var Stext = React.createClass ({
+  render : function(){
+      return <div>Go on a Wiki Adventure</div>
+  }
+});
 
 var Button = React.createClass({
   render: function () {
     return (
       <button
-        className="startbutton"
+        // className="startbutton"
         // style={startButtonStyle}
         onClick={this.props.handleClick}>Start Game</button>
     );
   }
 });
 
-
-ReactDOM.render(<Button/>, document.getElementById('startgame'));
+ReactDOM.render(<Stext/>, document.getElementById('startText'));
+ReactDOM.render(<Button/>, document.getElementById('startButton'));
 
 // module.exports = Button;
