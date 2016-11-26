f646ea07b86c910622f6f7966aecd8dc94b07367
rgalbiati <raina.galbiati@tufts.edu>
2016-11-26 15:01:29 -0500

    commented out 1st page stuff to start working on next page

--- public/react.jsx
+++ public/react.jsx
@@ -17,12 +17,13 @@ var StartGame = React.createClass({
       <div id="startText">Go on a Wiki Adventure</div>
       <div id="startButton"><button
         onClick={() => handleClick()}>Start Game</button></div>
+
         </div>
     );
   }
 });
 
-ReactDOM.render(<StartGame/>, document.getElementById('startgame'));
+// ReactDOM.render(<StartGame/>, document.getElementById('startgame'));
 
 //old line belongs in button div but I needed intermediary work first
 // onClick={this.props.handleClick}>Start Game</button></div>
