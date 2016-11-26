6836ed941fa07bec061b85b2daa588202aa6fa3f
rgalbiati <raina.galbiati@tufts.edu>
2016-11-26 15:31:20 -0500

    second page display is done, data must be sent somewhere, also I think maybe needs to be linked to separate url?

--- public/react.jsx
+++ public/react.jsx
@@ -23,6 +23,37 @@ var StartGame = React.createClass({
   }
 });
 
+
+var GetWords = React.createClass({
+  render: function () {
+    return (
+        <div className ="form"> 
+        <form action="">
+        Enter Stating Article:   <input type="text" name="firstWord" value="" />
+            <br />
+            <br />
+        Enter Ending Article:   <input type="text" name="firstWord" value="" />
+            <br />
+            <br />
+        Or 
+        <br />
+        <br />
+        Randomize:    <input type="checkbox" name="randomize" value="random" />
+        <br />
+        <br />
+            <button
+                onClick={() => handleClick()}>Start Game</button>
+        </form>
+        </div>
+      );
+  }
+
+});
+
+ReactDOM.render(<GetWords/>, document.getElementById('getwords'));
+
+
+
 // ReactDOM.render(<StartGame/>, document.getElementById('startgame'));
 
 //old line belongs in button div but I needed intermediary work first
