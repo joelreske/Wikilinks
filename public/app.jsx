b23249bf54487383229bd7dcc35779537f046355
Toby Glover <william.glover@tufts.edu>
2016-12-01 21:30:08 -0500

    began work on homepage

--- public/app.jsx
+++ public/app.jsx
@@ -1,17 +1,67 @@
-class NewGame extends React.Component{
+class NewGame extends React.Component {
     constructor(props) {
         super(props);
+        this.randomizeStartPage = this.randomizeStartPage.bind(this);
+        this.randomizeEndPage = this.randomizeEndPage.bind(this);
+        this.parseStartPageEntry = this.parseStartPageEntry.bind(this);
+        this.parseEndPageEntry = this.parseEndPageEntry.bind(this);
     }
 
     render() {
         return (
-            <form action="" className ="form">
-            Enter Stating Article: <input type="text" ref="firstWord"/>
-            Enter Ending Article: <input type="text" ref="secondWord"/>
-            <button type="submit"></button>
+            <div className="container">
+                <div className="form-group">
+                    <label htmlFor="startPage">Start Page:</label>
+                    <input className="input-sm" id="startPage" placeholder="Enter Start Page" onChange={this.parseStartPageEntry}/>
+                    <button className="btn btn-default" onClick={this.randomizeStartPage}>Randomize</button>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="endPage">End Page:</label>
+                    <input className="input-sm" id="endPage" placeholder="Enter End Page" onChange={this.parseEndPageEntry}/>
+                    <button className="btn btn-default" onClick={this.randomizeEndPage}>Randomize</button>
+                </div>
+                <button className="btn btn-default" onClick={this.startGame}>Start Game</button>
+            </div>
         );
+
     }
 
+    randomize(inputId) {
+        $.getJSON("/api/getRandomPage", function(data) {
+            $("#" + inputId).val(data.pageTitle);
+        });
+    }
+
+    randomizeStartPage(event) {
+        this.randomize("startPage");
+    }
+
+    randomizeEndPage() {
+        this.randomize("endPage");
+    }
+
+    parsePageEntry(inputId) {
+        var urlRegx = /(?:http:\/\/|https:\/\/)en.wikipedia.org\/wiki\/([^#<>[\]|{}]*)/i
+        var inputField = $("#" + inputId);
+        var match = urlRegx.exec(inputField.val());
+
+        if (match) {
+            var pagetitle = match[1].replace(/_/g, " ");
+            inputField.val(pagetitle);
+        }
+    }
+
+    parseEndPageEntry() {
+        this.parsePageEntry("endPage");
+    }
+
+    parseStartPageEntry() {
+        this.parsePageEntry("startPage");
+    }
+
+    startGame() {
+
+    }
 }
 
 class InGame extends React.Component{
@@ -34,7 +84,7 @@ class App extends React.Component {
     }
 
     render(){
-        if (true){
+        if (false){
             return (
                 <InGame gameId="1234"/>
             );
